3069069fee14636b58c46efad0c4ab3cd0c72825
Michael <160180509+m1ndb0x@users.noreply.github.com>
2024-10-31 11:35:20 -0400

    Revert "Revert "navigation, feed (unfunctional), styling edits""

---
+++ src/components/sidebar.js
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react';
+import { Button, Nav } from 'react-bootstrap';
+import '../components.css';
+import { UserContext } from "../helpers/UserContext";
+import { useNavigate } from 'react-router-dom';
+
+const Sidebar = () => {
+    const { setUser } = useContext(UserContext); // Access setUser from context
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        sessionStorage.removeItem("accessToken"); // Remove access token
+        setUser(null); // Clear user context
+        navigate("/login"); // Redirect to login page
+    };
+
+    const handleProfile =() => {
+        navigate("/profile");
+    }
+
+    return (
+        <div className="d-flex flex-column sidebar p-3" style={{ height: '100vh' }}>
+            <Nav className='flex-column mb-auto' style={{ overflowY: 'auto' }}>
+                <Button variant="outline-secondary" className="mb-3" onClick={handleProfile}>Profile</Button>
+                <Button variant="outline-secondary" className="mb-3">Home</Button>
+                <Button variant="outline-secondary" className="mb-3">Following</Button>
+                <Button variant="outline-secondary" className="mb-3">Upgrade</Button>
+            </Nav>
+            <Button variant="outline-danger" className="mt-3" onClick={handleLogout}>
+                Logout
+            </Button>
+        </div>
+    );
+};
+
+export default Sidebar;
