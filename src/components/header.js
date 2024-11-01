13fd4f0ed5337d0cced4ed4f343397c765a41b10
Michael <160180509+m1ndb0x@users.noreply.github.com>
2024-11-01 19:17:01 -0400

    added button on twixer logo to return to "/"

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Navbar, Form, FormControl, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import '../components.css';
 
 const Header = () => {
     return (
         <Navbar style={{ backgroundColor: '#106cfc', marginBottom: '0px' }} variant='dark' className='p-3'>
             <Container className='d-flex justify-content-between align-items-center'>
-                <Navbar.Brand className='logo mx-auto'>
-                    Twixer <span role='img' aria-label='bird'></span>
-                </Navbar.Brand>
+                {/* Wrap Navbar.Brand with Link */}
+                <Link to="/" className='logo mx-auto' style={{ textDecoration: 'none', color: 'white' }}>
+                    <Navbar.Brand>
+                        Twixer <span role='img' aria-label='bird'></span>
+                    </Navbar.Brand>
+                </Link>
                 <Form className='d-flex ml-auto'>
                     <FormControl 
                         type='text' 
