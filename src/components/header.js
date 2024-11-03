b540cc59888fd550a321198686d1fdb4c8aea2bb
Michael <160180509+m1ndb0x@users.noreply.github.com>
2024-11-03 12:21:16 -0500

    pagination and removed search in header

--- src/components/header.js
+++ src/components/header.js
@@ -7,19 +7,11 @@ const Header = () => {
     return (
         <Navbar style={{ backgroundColor: '#106cfc', marginBottom: '0px' }} variant='dark' className='p-3'>
             <Container className='d-flex justify-content-between align-items-center'>
-                {/* Wrap Navbar.Brand with Link */}
                 <Link to="/" className='logo mx-auto' style={{ textDecoration: 'none', color: 'white' }}>
                     <Navbar.Brand>
                         Twixer <span role='img' aria-label='bird'></span>
                     </Navbar.Brand>
                 </Link>
-                <Form className='d-flex ml-auto'>
-                    <FormControl 
-                        type='text' 
-                        placeholder='Search...' 
-                        className='mr-2 search-bar'
-                    />
-                </Form>
             </Container>
         </Navbar>
     );
